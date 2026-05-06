4c1d894ceb71aae44415b47ea666aceec78aef40
Yentrix <47158990+Yentrix@users.noreply.github.com>
2026-05-06 23:48:01 +0200

    Organización front Angular
    Creacion de las paginas basicas del frontend y sus rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { PetCreateComponent } from './pages/pet-create/pet-create.component';
+import { PostCreateComponent } from './pages/post-create/post-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent,
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'auth', component: AuthComponent },
+            { path: 'profile', component: ProfileComponent },
+            { path: 'pets', component: PetCreateComponent},
+            { path: 'posts', component: PostCreateComponent}
+        ]
+    }
+];
---
+++ frontend/src/app/pages/modules/pet-post/pet-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './pet-post.component.html',
+  styleUrl: './pet-post.component.css'
+})
+export class PetPostComponent {
+
+}
